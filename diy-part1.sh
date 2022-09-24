74d28614c85d6cfc6d6c0b00de9bf417ae622926
wuhui3009 <53723042+wuhui3009@users.noreply.github.com>
2022-09-24 19:18:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git netmaker http://github.com/sbilly/netmaker-openwrt.git' feeds.conf.default
